3f3a12cc64750cf574ee85a205ead7fe8593f30b
wxyz <hyhjwzx@126.com>
2015-09-27 20:52:30 +0800

    新增crmp-common项目和crmp-probe项目，并测试资源文件读取功能和打包的pom文件

---
+++ crmp-common/src/main/java/wxyz/crmp/common/StringUtils.java
@@ -0,0 +1,49 @@
+/**
+ * @Copyright 
+ * @Timestamp 2015-9-27 下午6:24:04
+ */
+package wxyz.crmp.common;
+
+/**
+ * <p>
+ * 字符串工具类
+ * </p>
+ * @Author wxyz
+ * @Timestamp 2015-9-27 下午6:24:04
+ */
+public class StringUtils {
+	
+	/**
+	 * <p>
+	 * 判断字符串是否为null或空，这个的空包括多个空格 <br>
+	 * 比如：<br>
+	 * StringUtil.isBlank(null) --> true <br>
+	 * StringUtil.isBlank("")  --> true <br>
+	 * StringUtil.isBlank("  ") --> true <br>
+	 * StringUtil.isBlank("abc ") --> false <br>
+	 * StringUtil.isBlank("a bc") --> false
+	 * <p>
+	 * @param str 字符串对象
+	 * @return 验证结果
+	 */
+	public static boolean isBlank(String str) {
+		return str == null || str.equals("") || str.trim().equals("");
+	}
+	
+	/**
+	 * <p>
+	 * 判断字符串是否为null或空 <br>
+	 * 比如：<br>
+	 * StringUtil.isBlank(null) --> true <br>
+	 * StringUtil.isBlank("")  --> true <br>
+	 * StringUtil.isBlank("  ") --> false <br>
+	 * StringUtil.isBlank("abc ") --> false <br>
+	 * StringUtil.isBlank("a bc") --> false
+	 * </p>
+	 * @param str 字符串对象
+	 * @return 验证结果
+	 */
+	public static boolean isEmpty(String str) {
+		return str == null || str.equals("");
+	}
+}
---
+++ crmp-probe/src/main/java/wxyz/crmp/probe/App.java
@@ -0,0 +1,43 @@
+package wxyz.crmp.probe;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	
+	public static void main(String[] args) {
+
+		URL resourceDir = App.class.getResource("/");
+		if (null != resourceDir) {
+			File f = new File(resourceDir.getFile() + "/test.txt");
+			if (f.exists()) {
+				System.out.println(f.getAbsolutePath());
+			}
+			else {
+				System.out.println("file not exist.");
+			}
+		}
+		else {
+			System.out.println("resources directory not exist.");
+		}
+		
+		InputStream is = App.class.getResourceAsStream("/test.txt");
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		try {
+			String tmpStr = br.readLine();
+			System.out.println(tmpStr);
+		}
+		catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+	}
+}
