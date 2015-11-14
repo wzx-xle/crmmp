da01544a7b4e3a996d16f1bf2ebedd570b749d40
wxyz <hyhjwzx@126.com>
2015-11-14 18:16:38 +0800

    新增机器信息获取接口

---
+++ crmp-probe/src/main/java/wxyz/crmp/probe/MachineInfo.java
@@ -0,0 +1,26 @@
+/**
+ * @Copyright 
+ * @Project crmp-probe
+ * @Package wxyz.crmp.probe
+ * @CreateTime 2015-11-14 下午6:02:57
+ */
+package wxyz.crmp.probe;
+
+/**
+ * <p>
+ * 机器信息接口
+ * </p>
+ * @Author wxyz
+ * @CreateTime 2015-11-14 下午6:02:57 init class
+ */
+public interface MachineInfo<T> {
+
+	/**
+	 * <p>
+	 * 机器信息
+	 * </p>
+	 * @return
+	 * @CreateTime 2015-11-14 下午6:04:03 create method
+	 */
+	T info();
+}
