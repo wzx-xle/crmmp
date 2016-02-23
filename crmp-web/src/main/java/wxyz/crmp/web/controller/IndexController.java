a91d0a1bc184a10200a058dafc62f726b3c8a892
wxyz <hyhjwzx@126.com>
2016-02-23 20:49:28 +0800

    添加web脚手架

---
+++ crmp-web/src/main/java/wxyz/crmp/web/controller/IndexController.java
@@ -0,0 +1,18 @@
+/**
+ * @Copyright 
+ * @Project crmp-web
+ * @Package wxyz.crmp.web.controller
+ * @CreateTime 2016-2-23 下午8:35:44
+ */
+package wxyz.crmp.web.controller;
+
+/**
+ * <p>
+ * 首页
+ * </p>
+ * @Author wxyz
+ * @CreateTime 2016-2-23 下午8:35:44 init class
+ */
+public class IndexController {
+
+}
